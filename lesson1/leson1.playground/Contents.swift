7b11dfd76c1511a33ed7e3aa9fc8eb90bb5bbab6
klintukhov <76727787+klintukhov@users.noreply.github.com>
2022-11-19 19:38:57 +0100

    Update Contents.swift

--- lesson1/leson1.playground/Contents.swift
+++ lesson1/leson1.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var name1 = "Alex"
+
+name1 = "Ivan"
+
+name1
+
+let name2 = "Oleg"
+
+name2
+
+print (name1)
+
+print ("Grisha")
+
+print (name1 + " " + name2)
+
+var colorRed = "red"
+ colorRed
